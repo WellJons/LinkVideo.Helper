fb267d2aac9ece9fd38c31a3c83198eef450bc06
WellJons <kromovnikita@gmail.com>
2026-09-06 21:30:43 +0700

    Apply UTC+7 deadline diagnostics

--- server/linkvideo_vpnsync/__init__.py
+++ server/linkvideo_vpnsync/__init__.py
@@ -6,6 +6,7 @@ from .db_cloud_read_compat import install_cloud_read_compat
 from .timezone_compat import install_business_timezone
 from .monitor_runtime_compat import install_monitor_runtime_compat
 from .activity_compat import install_activity_tracking
+from .deadline_timezone_compat import install_deadline_timezone_compat
 
 install_routeros_address_compat()
 install_nat_rule_compat()
@@ -13,3 +14,4 @@ install_cloud_read_compat()
 install_business_timezone()
 install_monitor_runtime_compat()
 install_activity_tracking()
+install_deadline_timezone_compat()
