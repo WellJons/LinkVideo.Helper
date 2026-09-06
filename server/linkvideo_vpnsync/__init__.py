b9a050d49839df2b7b8da26a3d7b98ca9700ae0d
WellJons <kromovnikita@gmail.com>
2026-09-06 20:17:04 +0700

    Enable UTC+3 lifecycle compatibility

--- server/linkvideo_vpnsync/__init__.py
+++ server/linkvideo_vpnsync/__init__.py
@@ -2,8 +2,10 @@
 
 from .db_routeros_address_compat import install_routeros_address_compat
 from .db_nat_rule_compat import install_nat_rule_compat
+from .timezone_compat import install_business_timezone
 from .monitor_runtime_compat import install_monitor_runtime_compat
 
 install_routeros_address_compat()
 install_nat_rule_compat()
+install_business_timezone()
 install_monitor_runtime_compat()
