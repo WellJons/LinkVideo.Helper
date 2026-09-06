aa79650ba070dd16fdaa6dfa3df5e00288592513
WellJons <kromovnikita@gmail.com>
2026-09-06 17:57:17 +0700

    Enable NAT rule identity compatibility

--- server/linkvideo_vpnsync/__init__.py
+++ server/linkvideo_vpnsync/__init__.py
@@ -1,7 +1,9 @@
 """LinkVideo.VPNSync server package."""
 
 from .db_routeros_address_compat import install_routeros_address_compat
+from .db_nat_rule_compat import install_nat_rule_compat
 from .monitor_runtime_compat import install_monitor_runtime_compat
 
 install_routeros_address_compat()
+install_nat_rule_compat()
 install_monitor_runtime_compat()
