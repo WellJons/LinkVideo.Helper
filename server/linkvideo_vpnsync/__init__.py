c9d23035d5b0d8ba7427a626c3abe7267d627e11
WellJons <kromovnikita@gmail.com>
2026-09-06 20:24:42 +0700

    Enable automatic VPNSync activity tracking

--- server/linkvideo_vpnsync/__init__.py
+++ server/linkvideo_vpnsync/__init__.py
@@ -4,8 +4,10 @@ from .db_routeros_address_compat import install_routeros_address_compat
 from .db_nat_rule_compat import install_nat_rule_compat
 from .timezone_compat import install_business_timezone
 from .monitor_runtime_compat import install_monitor_runtime_compat
+from .activity_compat import install_activity_tracking
 
 install_routeros_address_compat()
 install_nat_rule_compat()
 install_business_timezone()
 install_monitor_runtime_compat()
+install_activity_tracking()
