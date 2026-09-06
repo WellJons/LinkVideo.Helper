760dac6c972a0adcfb42782714cf082855f39a8d
WellJons <kromovnikita@gmail.com>
2026-09-06 17:00:34 +0700

    Install monitor runtime compatibility

--- server/linkvideo_vpnsync/__init__.py
+++ server/linkvideo_vpnsync/__init__.py
@@ -1,5 +1,7 @@
 """LinkVideo.VPNSync server package."""
 
 from .db_routeros_address_compat import install_routeros_address_compat
+from .monitor_runtime_compat import install_monitor_runtime_compat
 
 install_routeros_address_compat()
+install_monitor_runtime_compat()
